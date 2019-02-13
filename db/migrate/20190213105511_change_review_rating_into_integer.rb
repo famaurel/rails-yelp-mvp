d2ffdf563b6a73b9159029e6c6cc03c8fe98cfee
Fanny Maurel <fannymaurel@outlook.com>
2019-02-13 16:00:58 +0100

    Rails yelp

---
+++ db/migrate/20190213105511_change_review_rating_into_integer.rb
@@ -0,0 +1,5 @@
+class ChangeReviewRatingIntoInteger < ActiveRecord::Migration[5.2]
+  def change
+    change_column :reviews, :rating, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Review.destroy_all
+
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Team.creature,
+    address: Faker::Address.full_address,
+    category: 'french'
+  )
+  5.times do
+    Review.create!(
+      content: Faker::Team.name,
+      rating: Random.rand(0..5),
+      restaurant: restaurant
+    )
+  end
+end
